d44da400a91dd2f3bf10035a0d08f061347e582a
Nat Elliott-Green <natelliottgreen34@gmail.com>
2019-09-11 12:02:13 +0100

    Added method to store data in a .csv file

--- orig_directory.rb
+++ orig_directory.rb
@@ -1,5 +1,16 @@
 @students = [] # an empty array accessible to all methods
 
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  #iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+end
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -8,30 +19,10 @@ def input_students
   #while the name is not empty repeat thsis code
   while !name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: :november}
+    @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
     # get another name from the user
     name = gets.chomp
-  end
-  students
-end
-
-def show_students
-  print_header
-  print_students_list
-  print_footer(@students)
-end
-
-def process(selection)
-  case selection
-  when "1"
-    input_students
-  when "2"
-    show_students
-  when "9"
-    exit # this will cause the program to terminate
-  else
-    puts "I don't know what you meant, try again"
   end
 end
 
@@ -45,7 +36,29 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit" # 9 because we'll be adding more lines
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer(@students)
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "3"
+    save_students
+  when "9"
+    exit # this will cause the program to terminate
+  else
+    puts "I don't know what you meant, try again"
+  end
 end
 
 def print_header
@@ -53,15 +66,17 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
-  students.each_with_index do |student, i|
-    puts "#{i+1}: #{student[:name]} (#{student[:cohort]} cohort)"
+def print_student_list
+  @students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
 def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+  puts "Overall, we have #{@students.count} great students"
 end
+
+interactive_menu
 
 #nothing will happen until methods are called
 # print_header
